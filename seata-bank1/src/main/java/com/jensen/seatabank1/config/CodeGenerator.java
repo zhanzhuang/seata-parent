237d085bc3fbb44f4fcd77715d07a6eb595bb6b3
zhanzhuang <zhanzhuang0419@gmail.com>
2022-01-28 11:42:56 +0800

    commit

---
+++ seata-bank1/src/main/java/com/jensen/seatabank1/config/CodeGenerator.java
@@ -0,0 +1,31 @@
+package com.jensen.seatabank1.config;
+
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+
+import java.util.Collections;
+
+public class CodeGenerator {
+    static final String URL = "jdbc:mysql://localhost:3306/seata-bank1?characterEncoding=utf8&allowMultiQueries=true&useSSL=false&serverTimezone=Asia/Shanghai&allowPublicKeyRetrieval=true";
+
+    public static void main(String[] args) {
+        FastAutoGenerator.create(URL, "root", "123456")
+                .globalConfig(builder ->
+                        builder
+                                .fileOverride()
+                                .outputDir(System.getProperty("user.dir") + "/seata-bank1/src/main/java")
+                )
+                .packageConfig(builder ->
+                        builder
+                                .parent("")
+                                .moduleName("com.jensen.seatabank1")
+                                .pathInfo(Collections.singletonMap(OutputFile.mapperXml, System.getProperty("user.dir") + "/seata-bank1/src/main/resources/mapper"))
+                )
+                .strategyConfig(builder ->
+                        builder
+                                .addInclude("b1_bank")
+//                                .addTablePrefix("t_")
+                )
+                .execute();
+    }
+}
--- seata-bank1/src/test/java/com/jensen/seatabank1/SeataBank1ApplicationTests.java
+++ seata-bank1/src/test/java/com/jensen/seatabank1/SeataBank1ApplicationTests.java
@@ -1,7 +1,6 @@
 package com.jensen.seatabank1;
 
-import com.baomidou.mybatisplus.generator.FastAutoGenerator;
-import com.baomidou.mybatisplus.generator.config.OutputFile;
+
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
